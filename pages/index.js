489a398f40f8447cb160b120a6fa3c2687633bf6
rvale2014 <rvale2014@gmail.com>
2025-10-28 01:34:22 +0000

    Muda a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>O segredo do sucesso é a constância no objetivo!</h1>;
+    return <h1>O segredo do sucesso é a constância no objetivo! Vamos com tudo!</h1>;
 }
 export default Home; 
